9bad4c3237c55d914db9860156a63e97d94ac89c
Alexandria Rocks <alexandria.m.rocks@gmail.com>
2020-03-18 12:25:50 +0000

    Route num in destination test pass

---
+++ bus.rb
@@ -0,0 +1,19 @@
+class Bus
+
+  attr_reader :route_num, :destination, :passengers
+
+def initialize(route_num, destination)
+  @route_num = route_num
+  @destination = destination
+  @passengers = []
+end
+
+
+
+
+
+
+
+
+
+end
--- spec/bus_spec.rb
+++ spec/bus_spec.rb
@@ -1,11 +1,20 @@
 require('minitest/autorun')
 require('minitest/reporters')
-Minitest::Reporters.use! Minitest::Reporters::SpecReporter.newrequire('minitest/autorun')
+Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 
 require_relative("../bus")
 
-
-
-
-
 class BusTest < MiniTest::Test
+
+  def setup()
+    @bus = Bus.new(23, "City Centre")
+  end
+
+  def test_check_route_num()
+    assert_equal(23, @bus.route_num)
+  end
+
+  def test_check_destination()
+    assert_equal("City Centre", @bus.destination)
+  end
+end
